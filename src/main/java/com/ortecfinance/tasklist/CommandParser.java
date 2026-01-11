4f4cbc5ec70f7ad257897bf29b73255d698ce94f
Ryan Behari <snessgames@gmail.com>
2026-01-11 17:11:50 +0100

    finishing up command classes

--- src/main/java/com/ortecfinance/tasklist/CommandParser.java
+++ src/main/java/com/ortecfinance/tasklist/CommandParser.java
@@ -1,6 +1,9 @@
 package com.ortecfinance.tasklist;
 
+import java.time.LocalDate;
+
 public class CommandParser {
+
 
 
     public interface Command {}
@@ -25,5 +28,33 @@ public class CommandParser {
             this.projectName = projectName;
             this.taskDescription = taskDescription;
         }
+    }
+
+    public static class CheckCommand implements Command {
+        public final long taskId;
+        public final boolean done;
+        public CheckCommand(long taskId, boolean done) {
+            this.taskId = taskId;
+            this.done = done;
+        }
+    }
+
+    public static class DeadlineCommand implements Command {
+        public final long taskId;
+        public final LocalDate deadline;
+        public DeadlineCommand(long taskId, LocalDate deadline) {
+            this.taskId = taskId;
+            this.deadline = deadline;
+        }
+    }
+
+    public static class ErrorCommand implements Command {
+        public final String message;
+        public ErrorCommand(String message) { this.message = message; }
+    }
+
+    public static class UnknownCommand implements Command {
+        public final String command;
+        public UnknownCommand(String command) { this.command = command; }
     }
 }
